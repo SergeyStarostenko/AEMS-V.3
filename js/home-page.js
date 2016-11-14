3ee539eb9089e4915e189dbb68ce156c8b7b5278
SergeyStarostenko <sergeystarinternet@gmail.com>
2016-11-14 21:28:41 +0200

    add - new menu in home-page

--- js/home-page.js
+++ js/home-page.js
@@ -1,48 +1,113 @@
- $(function(){
-        $("#menu-bar-1").delay(1000).animate({marginLeft: "0"},"slow");
-        $("#menu-bar-2").delay(1500).animate({marginLeft: "0"},"slow");
-        $("#menu-bar-3").delay(2000).animate({marginLeft: "0"},"slow");
-        $("#menu-bar-4").delay(2500).animate({marginLeft: "0"},"slow");
-        $("#menu-bar-5").delay(3000).animate({marginLeft: "0"},"slow");
-        $('.button-menu').delay(3500).fadeIn(400);
-    });
-  function rotate(){
-    var deg = $(".button-menu").hasClass("rotate90");
-    if (deg == true){
-      $(".button-menu").removeClass("rotate90");
-      $(".button-menu").addClass("rotate180");
-      $(".sub-nav").hide(500);
-      $(".left_nav ul li button span").removeClass("rotate-90");
-            $("#menu-bar-5").delay(0).animate({marginLeft: "-180px"},"slow");
-            $("#menu-bar-4").delay(500).animate({marginLeft: "-180px"},"slow");
-            $("#menu-bar-3").delay(1000).animate({marginLeft: "-180px"},"slow");
-            $("#menu-bar-2").delay(1500).animate({marginLeft: "-180px"},"slow");
-            $("#menu-bar-1").delay(2000).animate({marginLeft: "-180px"},"slow");
-    }
-    if (deg == false){
-      $(".button-menu").removeClass("rotate180");
-      $(".button-menu").addClass("rotate90");
-        $(".sub-nav").hide(500);
-          $("#menu-bar-1").delay(0).animate({marginLeft: "0"},"slow");
-          $("#menu-bar-2").delay(500).animate({marginLeft: "0"},"slow");
-          $("#menu-bar-3").delay(1000).animate({marginLeft: "0"},"slow");
-          $("#menu-bar-4").delay(1500).animate({marginLeft: "0"},"slow");
-          $("#menu-bar-5").delay(2000).animate({marginLeft: "0"},"slow");
-    }
-  };
+$(document).ready(function(){
 
-function sub(numb){
-  if (numb == 2) {
-    if ($('.sub-nav').css('display') == 'none') 
-        { 
-            $('.sub-nav').show(500); 
-            $('#row2').addClass("rotate-90"); 
-        } 
-    else 
-        {     
-            $('.sub-nav').hide(500); 
-            $("#row2").removeClass("rotate-90");
-        } 
+$("#button-menu-open").click(function(){
+    $("#button-menu-open").toggleClass("display-block display-none");
+        console.log('[1.1]close->#button-menu-open==OK');
+    $(".left_nav").animate({left:'+=300'}, function(){
+        console.log('[1.2]left=+300->.left-nav==OK');
+    });
+    $(".left-nav-top").toggleClass("display-none display-block");
+        console.log('[1.3]open->.left-nav-top==OK');
+    openMenuBar();
+});
+
+$("#button-menu-close, .cd-slider-wrapper").click(function(){
+  if ($(".left-nav-top").hasClass("display-block")){
+    if ($("#sub-menu-students").hasClass("display-block")){
+        console.log($("#sub-menu-students").hasClass("display-block"));
+      $("#sub-menu-students").toggleClass("display-block display-none");
+        console.log('[3.3]close->#sub-menu-students==OK');
+    }
+    if ($("#sub-menu-students-top").hasClass("display-block")){
+        console.log($("#sub-menu-students-top").hasClass("display-block"));
+      $("#sub-menu-students-top").toggleClass("display-block display-none");
+        console.log('[3.4]close->#sub-menu-students-top==OK');
+    }
+    $("#sub-menu-students-img").toggleClass("display-block display-none");
+    console.log('[4.4]close->#sub-menu-students-img==OK')
+    $(".left-nav-top").toggleClass("display-block display-none");
+        console.log('[2.2]close->.left-nav-top==OK');
+    $(".left_nav").animate({left:'-=300'}, function(){
+        console.log('[2.3]left=-300->.left-nav==OK');
+    }); 
+    $("#button-menu-open").toggleClass("display-none display-block");
+        console.log('[2.4]open->#button-menu-open==OK');
+    closeMenuBar();
   }
-  
-};
+});
+
+$("#menu-bar-3-click").click(function(){
+    $("#left-nav-main").toggleClass("display-block display-none");
+    console.log('[3.1]close->#left-nav-main==OK')
+    $("#sub-menu-students").toggleClass("display-none display-block");
+    console.log('[3.2]open->#sub-menu-students==OK')
+    $("#sub-menu-students-top").toggleClass("display-none display-block");
+    console.log('[3.3]open->#sub-menu-students-top==OK')
+    $("#sub-menu-students-img").toggleClass("display-none display-block");
+    if ($("#sub-menu-students-img").hasClass("display-none")){
+      $("#sub-menu-students-img").toggleClass("display-none display-block");
+    }
+    console.log('[3.4]open->#sub-menu-students-img==OK')
+    heightSubMenuStudentsImg();
+});  
+
+$("#back-left-nav-main").click(function(){
+    $("#sub-menu-students-top").toggleClass("display-block display-none");
+    console.log('[4.1]close->#sub-menu-students-top==OK');
+    $("#sub-menu-students").toggleClass("display-block display-none");
+    console.log('[4.2]close->#sub-menu-students==OK');
+    $("#left-nav-main").toggleClass("display-none display-block");
+    console.log('[4.3]close->#left-nav-main==OK')
+    $("#sub-menu-students-img").toggleClass("display-block display-none");
+    console.log('[4.4]close->#sub-menu-students-img==OK')
+});
+
+});
+
+function openMenuBar(){
+  $("#left-nav-main").toggleClass("display-none display-block");
+      console.log('[1.4]open->#left-nav-main==OK');
+  if ($("#left-nav-main").hasClass("display-none")){
+    console.log($("#left-nav-main").hasClass("display-none"));
+    $("#left-nav-main").toggleClass("display-none display-block");
+      console.log('[1.5]close->#left-nav-main==OK');
+  }
+    positionMenuBar();
+}
+
+function closeMenuBar(){
+  $("#left-nav-main").toggleClass("display-block display-none");
+      console.log('[2.1]close->#left-nav-main==OK');
+}
+
+function positionMenuBar(){
+  var leftNavHeght = $(".left_nav").height();
+      console.log("! .left_nav = " + leftNavHeght);
+  var leftNavTopHeght = $(".left-nav-top").height();
+      console.log("! .left-nav-top = " + leftNavTopHeght);
+  var leftNavMainLiCount = $("#left-nav-main>li").length;
+      console.log("! кол-во -> #left-nav-main>li = " + leftNavMainLiCount);
+  var leftNavMainLiHeghtPadding = $("#left-nav-main>li").outerHeight(true);
+      console.log("! со всеми отступами -> #left-nav-main>li = " + leftNavMainLiHeghtPadding);
+  var leftNavMainLiHeght = $("#left-nav-main>li").height();
+      console.log("! без отступов -> #left-nav-main>li = " + leftNavMainLiHeght);
+  var paddingLeftNavMainLi = leftNavMainLiHeghtPadding - leftNavMainLiHeght;
+      console.log("! paddingLeftNavMainLi = " + paddingLeftNavMainLi);
+  var leftNavHeghtUl = leftNavHeght - leftNavTopHeght - (leftNavMainLiHeghtPadding * leftNavMainLiCount);
+      console.log("! leftNavHeghtUl = " + leftNavHeghtUl);
+  var topLeftNavMainUl = leftNavHeghtUl/2  + paddingLeftNavMainLi;
+      console.log("! topLeftNavMainUl = " + topLeftNavMainUl);
+      $("#left-nav-main").css('top', topLeftNavMainUl + 'px');
+          console.log('[1.5]top->positionMenuBar==OK');
+}
+
+function heightSubMenuStudentsImg(){
+  var leftNavHeght = $(".left_nav").height();
+      console.log("! .left_nav = " + leftNavHeght);
+  var leftNavTopHeght = $(".left-nav-top").height();
+      console.log("! .left-nav-top = " + leftNavTopHeght);
+  var subMenuStudentsTopHeght = $("#sub-menu-students-top").height();
+      console.log("! subMenuStudentsTopHeght = " + subMenuStudentsTopHeght);
+  var subMenuStudentsImgHeight = leftNavHeght - leftNavTopHeght - subMenuStudentsTopHeght;
+    $("#sub-menu-students-img").css({'height':subMenuStudentsImgHeight + 'px'});
+}
